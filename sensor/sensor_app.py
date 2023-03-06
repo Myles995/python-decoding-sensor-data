c73011f0abc9b683009a41ca1c7af90a85559823
Myles McDermott <cmt_myles@hotmail.com>
2023-03-06 11:18:41 +1030

    1.1

--- sensor/sensor_app.py
+++ sensor/sensor_app.py
@@ -1,20 +1,20 @@
-from load_data import load_sensor_data
-# Runner script for all modules
+# from load_data import load_sensor_data
+# # Runner script for all modules
 
 
-##############################
-# Do not remove these two lines
-# They are needed to validate your unittest
-data = [load_sensor_data()]
-print("Loaded records: {}".format(len(data)))
-##############################
+# ##############################
+# # Do not remove these two lines
+# # They are needed to validate your unittest
+# data = [load_sensor_data()]
+# print("Loaded records: {}".format(len(data)))
+# ##############################
 
-# Module 1 code here:
+# # Module 1 code here:
 
-# Module 2 code here:
+# # Module 2 code here:
 
-# Module 3 code here:
+# # Module 3 code here:
 
-# Module 4 code here:
+# # Module 4 code here:
 
-# Module 5 code here:
+# # Module 5 code here:
